ee1bbd21a65f9893dc5531a5b2459a1faf45b8ef
ErEnKaRaDuMaN <24181616773@gazi.edu.tr>
2024-12-20 15:04:36 +0300

    Add files via upload

---
+++ projealgoritma/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 30
+
+struct Yemek {
+    char isim[MAX_SIZE];
+    int kalori;
+};
+
+int main() {
+    //Gerekli değerleri girelim
+    struct Yemek yemekler[MAX_SIZE];
+    char siparis[MAX_SIZE];
+    int adet, toplam_kalori;
+    FILE *kalori_dosyasi, *siparis_dosyasi;
+
+    //Kalori dosyasını açalım
+    kalori_dosyasi = fopen("C:\\Users\\90541\\Desktop\\kalori.txt", "r");
+    if(kalori_dosyasi == NULL) {
+        printf("Dosya bulunamadi!");
+        return 1;
+    }
+    int i = 0;
+    while (fscanf(kalori_dosyasi, "%s %d", yemekler[i].isim, &yemekler[i].kalori) != EOF) {
+        i++;
+    }
+    fclose(kalori_dosyasi);
+
+    //Sipariş dosyasını açalım ve toplam kalori hesabını yapalım
+    siparis_dosyasi = fopen("C:\\Users\\90541\\Desktop\\siparis.txt", "r");
+    while (fscanf(siparis_dosyasi, "%s %d", siparis, &adet) != EOF) {
+        for (int j = 0; j < MAX_SIZE; j++) {
+            if (strcmp(siparis, yemekler[j].isim) == 0) {
+                toplam_kalori += yemekler[j].kalori * adet;
+                break;
+            }
+        }
+    }
+    fclose(siparis_dosyasi);
+
+    //Toplam kaloriyi ekrana yazdıralım
+    printf("Toplam kalori: %d\n", toplam_kalori);
+    return 0;
+}
